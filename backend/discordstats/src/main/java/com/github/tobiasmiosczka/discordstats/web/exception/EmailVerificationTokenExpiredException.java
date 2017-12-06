b85faef717e18921f9438fa1a926bf1a700e2a05
Tobias <6351397+TobiasMiosczka@users.noreply.github.com>
2017-12-06 04:47:58 +0100

    Added feature: reset/forgot password

--- backend/discordstats/src/main/java/com/github/tobiasmiosczka/discordstats/web/exception/EmailVerificationTokenExpiredException.java
+++
@@ -1,8 +0,0 @@
-package com.github.tobiasmiosczka.discordstats.web.exception;
-
-public class EmailVerificationTokenExpiredException extends Throwable {
-
-    public EmailVerificationTokenExpiredException(final String message) {
-        super(message);
-    }
-}
--- backend/discordstats/src/main/java/com/github/tobiasmiosczka/discordstats/web/exception/UnknownEmailVerificationTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.github.tobiasmiosczka.discordstats.web.exception;
-
-public class UnknownEmailVerificationTokenException extends Throwable {
-    public UnknownEmailVerificationTokenException(final String message) {
-        super(message);
-    }
-}
